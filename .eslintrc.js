a7462b5b1d2feeb6a924fdfe891140ebca21ba2b
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-02-11 14:36:58 +0100

    uzmena na eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "vue/script-indent": ['error', 4, { 'baseIndent' : 1}, 'tab']
   },
